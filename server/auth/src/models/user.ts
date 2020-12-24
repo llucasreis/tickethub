46fcf4ac71c518ebca78ced11216ffb2dd4fb93a
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2020-12-24 16:25:24 -0400

    feat: add password hash

--- server/auth/src/models/user.ts
+++ server/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // describe the properties to create a new User
 interface UserAttrs {
@@ -26,6 +27,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
+});
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    console.log(hashed);
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
