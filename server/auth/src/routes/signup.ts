4267cc5cf832c9ec4cc4f2ce09e5bd35587f83d5
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2020-12-25 20:34:35 -0400

    feat: create session with jwt

--- server/auth/src/routes/signup.ts
+++ server/auth/src/routes/signup.ts
@@ -1,27 +1,20 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .isLength({min: 4, max: 20})
-    .withMessage('Password must be between 4 and 20 characters')
+router.post('/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -33,6 +26,17 @@ router.post('/api/users/signup', [
     const user = User.build({ email, password });
     await user.save();
 
+    // kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+
+    req.session = {
+      jwt: userJwt
+    }
+    
     res.status(201).send(user);
 });
 
