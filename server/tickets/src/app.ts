87bf2cdd6149cfee3af4296b6ac1006acdd93cf2
Lucas Pereira Reis <lucasreis_15_@hotmail.com>
2021-01-17 10:29:44 -0400

    feat: create ShowTicket controller and test

--- server/tickets/src/app.ts
+++ server/tickets/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@lprs-tickethub/common';
+import { errorHandler, NotFoundError, currentUser } from '@lprs-tickethub/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +16,11 @@ app.use(
     // secure: true
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
